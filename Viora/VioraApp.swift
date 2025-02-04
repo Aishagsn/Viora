41d0756d37a0a675d9d5c1ed4e672621a9680745
Aisha Hasanguliyeva <ahasanguliyeva02@gmail.com>
2025-02-04 15:00:34 +0400

    Initial Commit

---
+++ Viora/VioraApp.swift
@@ -0,0 +1,17 @@
+//
+//  VioraApp.swift
+//  Viora
+//
+//  Created by Aisha on 04.02.25.
+//
+
+import SwiftUI
+
+@main
+struct VioraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VioraTests/VioraTests.swift
@@ -0,0 +1,17 @@
+//
+//  VioraTests.swift
+//  VioraTests
+//
+//  Created by Aisha on 04.02.25.
+//
+
+import Testing
+@testable import Viora
+
+struct VioraTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
